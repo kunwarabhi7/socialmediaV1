ece96c83b3ee3491133880190ba54973a38467f9
Abhishek <abhishekparmar162@gmail.com>
2022-10-03 23:39:18 +0530

    added post to firebase storage

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
 import DefaultLayout from '../Components/Layout/DefaultLayout'
 const AddPost = () => {
+
+  const addPost = () => {
+    const storage = getStorage();
+    const storageRef = ref(storage, `/posts/${image.name}`);
+    uploadBytes(storageRef, image).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    });
+  }
+
+
     const [image, setImage] = useState('')
     const [description, setDescription] = useState('')
 
@@ -13,8 +25,8 @@ const AddPost = () => {
 
 <h1 className='text-3xl font-bold text-red-700 text-center pt-4'>Add New Post</h1>
 <div className='flex flex-col'>
-<textarea name="" id="" cols="5" rows="5" className='ml-[40%] w-96 h-40 mt-4 border border-gray-500  '></textarea>
-<input className='ml-[45%] mt-4' type="file" onChange={e=>onImageChange(e)} />
+<textarea value={description} onChange={e=>setDescription(e.target.value)} cols="5" rows="5" className='ml-[40%] w-96 h-40 mt-4 border border-gray-500  '></textarea>
+<input className='ml-[45%] mt-4' type="file" onChange={ e=>onImageChange(e)} />
 
 {image && (
   <img src={URL.createObjectURL(image)} alt='image' className='w-80 h-80 mt-4 ml-[45%]' />
@@ -25,6 +37,11 @@ const AddPost = () => {
 
 </div>
     </div>
+
+    {description && image && (
+      <button className='bg-black text-white mt-8 w-30 h-10 ml-[55%] hover:bg-red-500 p-2' onClick={addPost} > Add Post</button>
+
+    )}
     </DefaultLayout>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -93,10 +93,10 @@ const signInUser = (e) => {
     
                         <div className="text-center text-sm text-grey-dark mt-4">
                             By signing up, you agree to the 
-                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="www.fb.com">
                                 Terms of Service
                             </a> and 
-                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            <a className="no-underline border-b border-grey-dark text-grey-dark" href="www.fb.com">
                                 Privacy Policy
                             </a>
                         </div>
